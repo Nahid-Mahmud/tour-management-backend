23622222298eed2b7933ce621c2a02b3b906d4bc
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-07 20:49:35 +0600

    Add morgan middleware for logging and update package.json with type definitions

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import morgan from "morgan";
 
 export const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
