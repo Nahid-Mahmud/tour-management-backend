7988d6c7335eeb23fffd1dfff4709147f7cf7a98
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-07 21:06:44 +0600

    Refactor server shutdown handling and improve error logging

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { app } from "./app";
 
 let server: Server;
 
-const startServer = async () => { 
+const startServer = async () => {
   if (!config.MONGO_URI) {
     console.error("MONGO_URI is not defined in the environment variables.");
     return;
@@ -27,3 +27,55 @@ const startServer = async () => {
 };
 
 startServer();
+
+//  handle unhandledRejection error // try  catch block error
+
+process.on("unhandledRejection", (error) => {
+  console.log("Unhandled Rejection at Promise, shutting down server...", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//uncaughtException error -- local error
+
+process.on("uncaughtException", (error) => {
+  console.log("Uncaught Exception, shutting down server...", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//  signal termination error -- server owner termination signal
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received, shutting down gracefully...");
+  if (server) {
+    server.close(() => {
+      console.log("Server closed");
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+});
+
+// manually close the server by user - sigint
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received, shutting down gracefully...");
+  if (server) {
+    server.close(() => {
+      console.log("Server closed");
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+});
