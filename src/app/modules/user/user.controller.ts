e667c9c2ee9d286f8ce8a2480ea2469776d01ecf
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-09 00:31:12 +0600

    Refactor user creation logic: move user creation from controller to user service for better separation of concerns

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,14 +1,10 @@
 import { Request, Response } from "express";
 import StatusCodes from "http-status-codes";
-import User from "./user.model";
+import { userServices } from "./user.service";
 
 const crateUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
-    const user = await User.create({
-      name,
-      email,
-    });
+    const user = await userServices.creteUser(req.body);
     res.status(StatusCodes.CREATED).json({
       message: "User created successfully",
       user,
