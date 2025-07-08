98e6abd9e947c7929413f1f4b46b55cb71187827
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-09 00:13:52 +0600

    Add 404 error handling: implement middleware for unmatched routes with appropriate status response

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import morgan from "morgan";
 import { router } from "./app/modules/routes";
 
+import StatusCodes from "http-status-codes";
+
 export const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
@@ -14,5 +16,14 @@ app.get("/", (req: Request, res: Response) => {
   res.json({
     message: "Welcome to the Tour Management API",
     status: "success",
+  });
+});
+
+// route not match
+
+app.use((req: Request, res: Response) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    message: "Route not found",
+    status: "failed",
   });
 });
