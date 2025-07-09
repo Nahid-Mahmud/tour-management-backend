adc3513231836f3d054756299285a0f8e95400f2
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-09 13:57:41 +0600

    Refactor user controller and service: implement sendResponse utility for consistent response structure and update getAllUsers method to return metadata

---
+++ src/utils/sendResponse.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+
+interface TResponse<T> {
+  success: boolean;
+  message: string;
+  data: T;
+  statusCode: number;
+  meta?: {
+    page?: number;
+    limit?: number;
+    total?: number;
+  };
+}
+
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+    meta: data.meta,
+    statusCode: data.statusCode,
+  });
+};
+
+export default sendResponse;
