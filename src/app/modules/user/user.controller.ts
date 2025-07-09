94601f45cf38726a6dd7dc3b6139d7ff82afa237
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-09 13:15:43 +0600

    Refactor user controller: implement catchAsync utility for error handling and fix createUser method name

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,34 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import StatusCodes from "http-status-codes";
+import { catchAsync } from "../../../utils/catchAsync";
 import { userServices } from "./user.service";
 
-const crateUser = async (req: Request, res: Response, next: NextFunction) => {
-  // throw new AppError(StatusCodes.BAD_REQUEST, "Test Error"); // This line is for testing the global error handler
-  try {
-    const user = await userServices.creteUser(req.body);
-    res.status(StatusCodes.CREATED).json({
-      message: "User created successfully",
-      user,
-    });
-  } catch (error: unknown) {
-    // res.status(StatusCodes.BAD_REQUEST).json({ message: "Internal Server Error", error });
-    next(error); // Pass the error to the global error handler
-  }
-};
+const createUser = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  const user = await userServices.creteUser(req.body);
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    message: "User created successfully",
+    user,
+  });
+});
 
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const users = await userServices.getAllUsers();
-    res.status(StatusCodes.OK).json({
-      message: "Users retrieved successfully",
-      users,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const getAllUsers = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
+  const users = await userServices.getAllUsers();
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Users retrieved successfully",
+    users,
+  });
+});
 
 export const userControllers = {
-  crateUser,
+  createUser,
   getAllUsers,
 };
