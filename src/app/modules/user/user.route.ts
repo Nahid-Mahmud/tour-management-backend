e5652d7a658f4835d8c2ba56531ff93d126ff121
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-09 13:41:29 +0600

    Refactor user controller and routes: update response structure in getAllUsers method to include 'data' key

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,5 +4,6 @@ import { userControllers } from "./user.controller";
 const router = Router();
 
 router.post("/register", userControllers.createUser);
+router.get("/", userControllers.getAllUsers);
 
 export const userRoutes = router;
