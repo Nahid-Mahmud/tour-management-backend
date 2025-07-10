2d088f4cd3d7308c5c3933e4b501414d8edd7f65
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-11 00:44:46 +0600

    Add bcryptjs for password hashing and update creteUser function to include password handling

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,10 +1,11 @@
+import bcryptjs from "bcryptjs";
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "./user.interface";
+import { IAuthProvider, IUser } from "./user.interface";
 import User from "./user.model";
 import { StatusCodes } from "http-status-codes";
 
 const creteUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email, password, ...rest } = payload;
 
   // check if user already exists
   const existingUser = await User.findOne({ email });
@@ -12,8 +13,17 @@ const creteUser = async (payload: Partial<IUser>) => {
     throw new AppError(StatusCodes.CONFLICT, "User already exists with this email");
   }
 
+  const hashedPassword = await bcryptjs.hash(password as string, 10);
+
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string, // using type assertion to ensure providerId is a string. Because Email is guaranteed to be a string because of using zod validation.
+  };
+
   const user = await User.create({
     email,
+    password: hashedPassword,
+    auths: [authProvider],
     ...rest,
   });
   return user;
