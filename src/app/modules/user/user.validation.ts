d1c924141d8270079e2e047e609c5d74b213a411
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-10 23:44:02 +0600

    Add user validation schema for updating user details with optional fields

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { IsActive } from "./user.interface";
 
 export const createUserSchema = z.object({
   name: z
@@ -27,4 +28,27 @@ export const createUserSchema = z.object({
   phone: z.string().regex(/^(?:\+880|880|0)(1[3-9])[0-9]{8}$/, {
     message: `Phone number must be a valid Bangladeshi mobile number (e.g., +8801XXXXXXXXX, 8801XXXXXXXXX, or 01XXXXXXXXX) `,
   }),
+});
+
+export const updateUserSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters long" })
+    .max(50, { message: "Name must not exceed 50 characters" })
+    .optional(),
+  email: z
+    .string()
+    .email({ message: "Invalid email format" })
+    .max(100, { message: "Email must not exceed 100 characters" })
+    .optional(),
+  phone: z
+    .string()
+    .regex(/^(?:\+880|880|0)(1[3-9])[0-9]{8}$/, {
+      message: `Phone number must be a valid Bangladeshi mobile number (e.g., +8801XXXXXXXXX, 8801XXXXXXXXX, or 01XXXXXXXXX) `,
+    })
+    .optional(),
+  address: z.string().max(255, { message: "Address must not exceed 255 characters" }).optional(),
+  isDeleted: z.boolean().optional(),
+  isActive: z.enum(Object.values(IsActive)).optional(),
+  isVerified: z.boolean().optional(),
 });
