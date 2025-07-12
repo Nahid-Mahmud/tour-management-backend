e6d3e770036dc71eb87753e6aa72230678055b4e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-12 23:52:02 +0600

    Add seedSuperAdmin utility for seeding super admin user with credentials

---
+++ src/utils/seedSuperAdmin.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+import envVariables from "../app/config/env";
+import { IAuthProvider, IUser, UserRole } from "../app/modules/user/user.interface";
+import User from "../app/modules/user/user.model";
+import { hashPassword } from "./hashPassword";
+
+export const seedSuperAdmin = async () => {
+  try {
+    // console.log("object");
+
+    // check if super admin already exists
+    const existingSuperAdmin = await User.findOne({ email: envVariables.SUPER_ADMIN_EMAIL });
+
+    if (existingSuperAdmin) {
+      console.log("Super admin already exists.");
+      return;
+    }
+
+    const password = await hashPassword(envVariables.SUPER_ADMIN_PASSWORD);
+
+    const authProvider: IAuthProvider = {
+      provider: "credentials",
+      providerId: envVariables.SUPER_ADMIN_EMAIL as string,
+    };
+
+    const payload: Partial<IUser> = {
+      name: "Super Admin",
+      email: envVariables.SUPER_ADMIN_EMAIL,
+      role: UserRole.SUPER_ADMIN,
+      password,
+      isVerified: true,
+      auths: [authProvider],
+    };
+
+    await User.create(payload);
+    console.log("Super admin seeded successfully.");
+  } catch (error) {
+    console.error("Error seeding super admin:", error);
+  }
+};
