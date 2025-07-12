51efcc52db2dcb71546bab4781d5627d50477178
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-12 17:03:06 +0600

    Implement authorization middleware for user access control in getAllUsers route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,11 +1,42 @@
-import { Router } from "express";
-import { userControllers } from "./user.controller";
+import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import envVariables from "../../config/env";
+import AppError from "../../errorHelpers/AppError";
 import { validateRequest } from "../../middlewares/validateRequest";
+import { userControllers } from "./user.controller";
+import { UserRole } from "./user.interface";
 import { createUserSchema } from "./user.validation";
 
 const router = Router();
 
 router.post("/register", validateRequest(createUserSchema), userControllers.createUser);
-router.get("/", userControllers.getAllUsers);
+router.get(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+      if (!accessToken) {
+        throw new AppError(StatusCodes.UNAUTHORIZED, "Access token is required");
+      }
+      const verifiedToken = jwt.verify(accessToken, envVariables.JWT_SECRET);
+
+      const role = (verifiedToken as JwtPayload).role;
+
+      //   if (role !== UserRole.ADMIN || role !== UserRole.SUPER_ADMIN) {
+      //     throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to access this resource");
+      //   }
+
+      if (role !== UserRole.ADMIN) {
+        throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to access this resource");
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  userControllers.getAllUsers
+);
 
 export const userRoutes = router;
