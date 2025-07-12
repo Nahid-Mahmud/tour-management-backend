5b3e52925be2e9f91c49a9c069a6f64e2f38c781
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-12 17:03:16 +0600

    Refactor credentialLogin to use response object for cookie and user data

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,12 +5,19 @@ import { authServices } from "./auth.service";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const credentialLogin = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
-  const user = await authServices.credentialLogin(req.body);
+  const response = await authServices.credentialLogin(req.body);
+
+  res.cookie("accessToken", response.accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000,
+  }); 
 
   sendResponse(res, {
     success: true,
     message: "User logged in successfully",
-    data: user,
+    data: response.user,
     statusCode: 200,
   });
 });
