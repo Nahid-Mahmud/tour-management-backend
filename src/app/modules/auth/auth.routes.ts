6f88d4e986446e6fc4cc3f5fdf9caa06d4bb438b
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-15 02:42:25 +0600

    Refactor authentication routes: update refresh token method name, add Google authentication route, and improve code organization

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,13 +1,22 @@
-import { Router } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response, Router } from "express";
 import { authControllers } from "./auth.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { UserRole } from "../user/user.interface";
+import passport from "passport";
 
 const route = Router();
 
 route.post("/login", authControllers.credentialLogin);
-route.post("/refresh-token", authControllers.generateAuthTokens);
+route.post("/refresh-token", authControllers.generateAccessTokensUsingRefreshToken);
 route.post("/logout", authControllers.logout);
 route.patch("/reset-password", checkAuth(...Object.values(UserRole)), authControllers.resetPassword);
+route.get("/google", async (req: Request, res: Response, _next: NextFunction) => {
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })(req, res);
+});
+
+route.get("google/callback", authControllers.googleCallback);
 
 export const authRoutes = route;
