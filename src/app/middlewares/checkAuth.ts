7ab47dab4f8e5847057122d968f304bcc3c6293e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-15 02:42:17 +0600

    Enhance authentication middleware: add user existence checks and status validation

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -3,7 +3,8 @@ import { StatusCodes } from "http-status-codes";
 import { verifyToken } from "../../utils/jwt";
 import envVariables from "../config/env";
 import AppError from "../errorHelpers/AppError";
-import { UserRole } from "../modules/user/user.interface";
+import { IsActive, UserRole } from "../modules/user/user.interface";
+import User from "../modules/user/user.model";
 
 export const checkAuth =
   (...authRoles: UserRole[]) =>
@@ -15,8 +16,19 @@ export const checkAuth =
       }
       const verifiedToken = verifyToken(accessToken, envVariables.ACCESS_TOKEN_JWT_SECRET);
 
-      const role = verifiedToken.role;
-      if (!authRoles.includes(role)) {
+      const isUserExist = await User.findOne({ email: verifiedToken.email });
+
+      if (!isUserExist) {
+        throw new AppError(StatusCodes.BAD_REQUEST, "User does not exist");
+      }
+      if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
+        throw new AppError(StatusCodes.BAD_REQUEST, `User is ${isUserExist.isActive}`);
+      }
+      if (isUserExist.isDeleted) {
+        throw new AppError(StatusCodes.BAD_REQUEST, "User is deleted");
+      }
+
+      if (!authRoles.includes(verifiedToken.role)) {
         throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to access this resource");
       }
 
