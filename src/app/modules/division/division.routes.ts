a1a49ba8c7480756f13faed9679cb2a75564ff88
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-17 19:38:13 +0600

    Fix division routes: import updateDivisionSchema for validation in updateDivision method

--- src/app/modules/division/division.routes.ts
+++ src/app/modules/division/division.routes.ts
@@ -3,7 +3,7 @@ import { checkAuth } from "../../middlewares/checkAuth";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { UserRole } from "../user/user.interface";
 import { divisionControllers } from "./division.controller";
-import { createDivisionSchema } from "./division.validaion";
+import { createDivisionSchema, updateDivisionSchema } from "./division.validaion";
 
 const router = Router();
 
@@ -13,6 +13,13 @@ router.post(
   checkAuth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(createDivisionSchema),
   divisionControllers.createDivision
+);
+
+router.patch(
+  "/:id",
+  checkAuth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  validateRequest(updateDivisionSchema),
+  divisionControllers.updateDivision
 );
 
 export const divisionRoutes = router;
