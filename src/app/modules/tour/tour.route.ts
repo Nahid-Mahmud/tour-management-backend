4809777ac98217829e1c8ced9af556203b47fa31
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-18 16:40:46 +0600

    Add tour routes with authentication for CRUD operations on tour types

--- src/app/modules/tour/tour.route.ts
+++ src/app/modules/tour/tour.route.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { TourController } from "./tour.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { UserRole } from "../user/user.interface";
 
 const router = Router();
 
+router.post("/create-tour-type", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), TourController.createTourType);
+router.patch("/tour-types/:id", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), TourController.editTourType);
+router.get("/tour-types", TourController.getAllTourTypes);
+router.delete("/tour-types/:id", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), TourController.deleteTourType);
 export const TourRoutes = router;
