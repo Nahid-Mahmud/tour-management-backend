b17793236f641308e3f68f97168f8e0ce399cdfc
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-23 00:21:15 +0600

    Implement createBooking function with validation and payment processing

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,5 +1,72 @@
-const createBooking = async () => {
-  return {};
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errorHelpers/AppError";
+import User from "../user/user.model";
+import { BOOKING_STATUS, IBooking } from "./booking.interface";
+import Booking from "./booking.model";
+import Payment from "../payment/payment.model";
+import { PAYMENT_STATUS } from "../payment/payment.interface";
+import crypto from "crypto";
+import { Tour } from "../tour/tour.model";
+
+// function to crate a unique transaction ID
+const getTransactionId = () => {
+  const date = Date.now();
+  const randomNumber = Math.floor(Math.random() * 1000);
+  const cryptoId = crypto.randomBytes(16).toString("hex");
+  return `tran_${date}_${randomNumber}_${cryptoId}`;
+};
+
+const createBooking = async (payload: Partial<IBooking>, userId: string) => {
+  // validate payload
+  if (!payload.guestCount || payload.guestCount <= 0) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "Guest count must be a positive integer");
+  }
+
+  const transactionId = getTransactionId();
+
+  const user = await User.findById(userId);
+
+  // check for phone and address
+  if (!user?.phone || !user?.address) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "Please Update you profile with phone and address");
+  }
+
+  const tour = await Tour.findById(payload.tour).select("constFrom");
+
+  if (!tour?.constFrom) {
+    throw new AppError(StatusCodes.NOT_FOUND, "Tour not found");
+  }
+
+  const amount = Number(tour.constFrom) * Number(payload.guestCount);
+
+  const booking = await Booking.create({
+    user: userId,
+    status: BOOKING_STATUS.PENDING,
+    ...payload,
+  });
+
+  const payment = await Payment.create({
+    booking: booking._id,
+    status: PAYMENT_STATUS.UNPAID,
+    transactionId: transactionId,
+    amount: amount,
+  });
+
+  const updatedBooking = await Booking.findByIdAndUpdate(
+    booking._id,
+    {
+      payment: payment._id,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .populate("user", "name email phone address")
+    .populate("tour", "title constFrom")
+    .populate("payment");
+
+  return updatedBooking;
 };
 
 const getUserBookings = async () => {
