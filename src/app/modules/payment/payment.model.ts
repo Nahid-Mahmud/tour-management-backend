903f7e1dc0674db77bb9d4b6c536204d4ab105f6
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-23 00:21:19 +0600

    Refactor payment schema formatting for improved readability

--- src/app/modules/payment/payment.model.ts
+++ src/app/modules/payment/payment.model.ts
@@ -1,35 +1,41 @@
 import { model, Schema } from "mongoose";
 import { IPayment, PAYMENT_STATUS } from "./payment.interface";
 
-const paymentSchema = new Schema<IPayment>({
-  booking: {
-    type: Schema.Types.ObjectId,
-    ref: "Booking",
-    required: true,
-    unique: true,
-  },
-  transactionId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: Object.values(PAYMENT_STATUS),
-    default: PAYMENT_STATUS.UNPAID,
-  },
+const paymentSchema = new Schema<IPayment>(
+  {
+    booking: {
+      type: Schema.Types.ObjectId,
+      ref: "Booking",
+      required: true,
+      unique: true,
+    },
+    transactionId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(PAYMENT_STATUS),
+      default: PAYMENT_STATUS.UNPAID,
+    },
 
-  paymentGatewayData: {
-    type: Schema.Types.Mixed,
+    paymentGatewayData: {
+      type: Schema.Types.Mixed,
+    },
+    invoiceUrl: {
+      type: String,
+    },
   },
-  invoiceUrl: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 const Payment = model<IPayment>("Payment", paymentSchema);
 export default Payment;
