3e7908cf57914d9770d9620ef1d41dbfc9adf97f
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-22 22:55:09 +0600

    Implement booking controller and routes with CRUD operations and validation

--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -1,6 +1,35 @@
 import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { UserRole } from "../user/user.interface";
+import { BookingController } from "./booking.controller";
+import { createBookingZodSchema, updateBookingZodSchema } from "./booking.validation";
 
 const router = Router();
+
+router.post(
+  "/",
+  checkAuth(...Object.values(UserRole)),
+  validateRequest(createBookingZodSchema),
+  BookingController.createBooking
+);
+
+// api/v1/booking
+router.get("/", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), BookingController.getAllBookings);
+
+// api/v1/booking/my-bookings
+router.get("/my-bookings", checkAuth(...Object.values(UserRole)), BookingController.getUserBookings);
+
+// api/v1/booking/bookingId
+router.get("/:bookingId", checkAuth(...Object.values(UserRole)), BookingController.getSingleBooking);
+
+// api/v1/booking/bookingId/status
+router.patch(
+  "/:bookingId/status",
+  checkAuth(...Object.values(UserRole)),
+  validateRequest(updateBookingZodSchema),
+  BookingController.updateBookingStatus
+);
 
 const bookingRoute = router;
 
