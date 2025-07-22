bf6799e80a8091f4d13bda2b9b081e81c70e054e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-22 21:43:52 +0600

    Add booking model with schema definition and validation

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+import { BOOKING_STATUS, IBooking } from "./booking.interface";
+
+const bookingSchema = new Schema<IBooking>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tour: {
+      type: Schema.Types.ObjectId,
+      ref: "Tour",
+      required: true,
+    },
+    payment: {
+      type: Schema.Types.ObjectId,
+      ref: "Payment",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(BOOKING_STATUS),
+      default: BOOKING_STATUS.PENDING,
+    },
+    guestCount: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Booking = model<IBooking>("Booking", bookingSchema);
+
+export default Booking;
