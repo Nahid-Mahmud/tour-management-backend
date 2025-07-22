f74179c5b12bbdd8c25b5de33d08b0bd4421d1d4
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-22 22:45:53 +0600

    Add booking service methods for creating and retrieving bookings Placeholder Functions to work on later

---
+++ src/app/modules/booking/booking.service.ts
@@ -0,0 +1,24 @@
+const createBooking = async () => {
+  return {};
+};
+
+const getUserBookings = async () => {
+  return {};
+};
+
+const getBookingById = async () => {
+  return {};
+};
+const updateBookingStatus = async () => {
+  return {};
+};
+const getAllBookings = async () => {
+  return {};
+};
+export const BookingService = {
+  createBooking,
+  getUserBookings,
+  getBookingById,
+  updateBookingStatus,
+  getAllBookings,
+};
