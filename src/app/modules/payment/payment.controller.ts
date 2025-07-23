d78ebc181485fa730a3d315e60b2ff97d39f2b37
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-23 23:00:35 +0600

    Implement success payment handling and redirect to SSL success URL

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,8 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
+import { paymentService } from "./payment.service";
+import envVariables from "../../config/env";
 
 const successPayment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const query = req.query;
+
+  const result = await paymentService.successPayment(query as Record<string, string>);
+
+  if (result.success) {
+    res.redirect(
+      `${envVariables.SSL.SSL_SUCCESS_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
+
   //
 });
 
