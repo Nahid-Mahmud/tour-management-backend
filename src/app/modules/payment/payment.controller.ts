892004f69daa5a5aa2ae20a948011d8bdfe9feed
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-24 00:20:58 +0600

    Refactor fail and cancel payment handling to include query parameters for improved redirection

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -20,10 +20,28 @@ const successPayment = catchAsync(async (req: Request, res: Response, next: Next
 
 const failPayment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   //
+  const query = req.query;
+
+  const result = await paymentService.failPayment(query as Record<string, string>);
+
+  if (!result.success) {
+    res.redirect(
+      `${envVariables.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
 });
 
 const cancelPayment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   //
+  const query = req.query;
+
+  const result = await paymentService.cancelPayment(query as Record<string, string>);
+
+  if (!result.success) {
+    res.redirect(
+      `${envVariables.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
 });
 
 export const paymentController = {
