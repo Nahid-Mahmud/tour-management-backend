a94a61acf42f9bd0286fbbe1aa3b4b20f5b8cff9
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-24 17:37:36 +0600

    Add handling for cases where request body is wrapped in 'data' in validateRequest middleware

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -4,6 +4,7 @@ import { ZodObject, ZodRawShape } from "zod";
 export const validateRequest =
   (ZodSchema: ZodObject<ZodRawShape>) => async (req: Request, res: Response, next: NextFunction) => {
     try {
+      req.body = req.body.data ? JSON.parse(req.body.data) : req.body; // Handle cases where body is wrapped in 'data'
       req.body = await ZodSchema.parseAsync(req.body);
       next();
     } catch (error) {
