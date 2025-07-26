18edf11806a0a314d5e4dc3360dfec9928f5d068
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-26 18:16:23 +0600

    Add OTP controller with send and verify functionality

---
+++ src/app/modules/otp/otp.controller.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { otpServices } from "./otp.service";
+import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+const sendOtp = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  const { email, name } = req.body;
+
+  await otpServices.sendOtp(email, name);
+  sendResponse(res, {
+    success: true,
+    message: "OTP sent successfully",
+    statusCode: StatusCodes.OK,
+    data: null,
+  });
+});
+
+const verifyOtp = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  await otpServices.verifyOtp();
+  sendResponse(res, {
+    success: true,
+    message: "OTP verified successfully",
+    statusCode: StatusCodes.OK,
+    data: null,
+  });
+});
+
+export const otpController = {
+  sendOtp,
+  verifyOtp,
+};
