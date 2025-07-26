fcbb93918900cd72da320c473d04b4f09ae72c1a
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-26 19:08:14 +0600

    Refactor OTP service to include user verification checks in sendOtp and verifyOtp functions

--- src/app/modules/otp/otp.service.ts
+++ src/app/modules/otp/otp.service.ts
@@ -1,6 +1,9 @@
 import crypto from "crypto";
 import { redisClient } from "../../config/redis.config";
 import { sendEmail } from "../../utils/sendEmail";
+import AppError from "../../errorHelpers/AppError";
+import { StatusCodes } from "http-status-codes";
+import User from "../user/user.model";
 
 const OTP_EXPIRATION_TIME = 2 * 60; // 2 minutes in seconds
 
@@ -10,6 +13,14 @@ const generateOtp = (length = 6): string => {
 };
 
 const sendOtp = async (email: string, name: string) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  if (user.isVerified) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "User is already verified");
+  }
+
   const otp = generateOtp();
   // store otp in redis
   const redisKey = `otp:${email}`;
@@ -31,8 +42,25 @@ const sendOtp = async (email: string, name: string) => {
   });
 };
 
-const verifyOtp = async () => {
-  //
+const verifyOtp = async (email: string, otp: string) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  if (user.isVerified) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "User is already verified");
+  }
+
+  const redisKey = `otp:${email}`;
+  const savedOtp = await redisClient.get(redisKey);
+  if (!savedOtp) {
+    throw new AppError(StatusCodes.UNAUTHORIZED, "OTP has expired or does not exist");
+  }
+  if (savedOtp !== otp) {
+    throw new AppError(StatusCodes.UNAUTHORIZED, "Invalid OTP");
+  }
+
+  await Promise.all([User.updateOne({ email }, { isVerified: true }), redisClient.del(redisKey)]);
 };
 
 export const otpServices = {
