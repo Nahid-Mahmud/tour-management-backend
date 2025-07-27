db94d49e0ccf48b2be427dd7294086d94dd653cb
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-27 19:05:25 +0600

    Add invoice download route with authentication check

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { paymentController } from "./payment.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { UserRole } from "../user/user.interface";
 
 const router = Router();
 
@@ -7,6 +9,7 @@ router.post("/init-payment/:bookingId", paymentController.initPayment);
 router.post("/success", paymentController.successPayment);
 router.post("/fail", paymentController.failPayment);
 router.post("/cancel", paymentController.cancelPayment);
+router.get("/invoice/:paymentId", checkAuth(...Object.values(UserRole)), paymentController.getInvoiceDownloadUrl);
 // router.post("/validate-request", paymentController.validateRequest);
 
 const paymentRoutes = router;
